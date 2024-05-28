b114f386108df2d85aca08d092e34ce10cc17fd9
bjhodgson <bjhodgson@ucdavis.edu>
2024-05-27 17:42:19 -0700

    More hists, summary stats, chi squared

--- median_barriers/R/CROS_medians.R
+++ median_barriers/R/CROS_medians.R
@@ -44,7 +44,7 @@ cleaned_df <- combined_df %>% select(-one_of(columns_to_remove))
 # Filter out NA values from the cleaned dataframe
 cleaned_df_filtered <- cleaned_df[!is.na(cleaned_df$New_ID_1), ]
 
-# Visualize histogram of hits per median type
+# Visualize histogram of hits per median pair type
 cleaned_df_filtered %>%
   group_by(Pair_Type) %>%
   summarise(count = n()) %>%
@@ -52,6 +52,13 @@ cleaned_df_filtered %>%
   geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.7) +
   labs(title = "Frequency of 'New_ID_1' Values", x = "Value", y = "Count") +
   theme_minimal()
+
+# Create list of excluded median types
+exclude_values <- c("tran")
+
+# Filter out excluded median types
+cleaned_df_filtered <- cleaned_df_filtered %>%
+  filter(!New_ID_1 %in% exclude_values)
 
 # Add concrete median binary variable
 cleaned_df_filtered$concrete_median <- as.factor(ifelse(cleaned_df_filtered$New_ID_1 == "con", 1, 0))
@@ -62,9 +69,8 @@ cleaned_df_filtered %>%
   summarise(count = n()) %>%
   ggplot(aes(x = concrete_median, y = count)) +
   geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Frequency of Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
+  labs(title = "Number of Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
   theme_minimal()
-
 
 # Visualize histogram of deer hits per concrete vs non-concrete median type
 cleaned_df_filtered %>%
@@ -73,7 +79,7 @@ cleaned_df_filtered %>%
   summarise(count = n()) %>%
   ggplot(aes(x = concrete_median, y = count)) +
   geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.7) +
-  labs(title = "Frequency of Deer Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
+  labs(title = "Number of Deer Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
   theme_minimal()
 
 # Visualize histogram of coyote hits per concrete vs non-concrete median type
@@ -83,5 +89,47 @@ cleaned_df_filtered %>%
   summarise(count = n()) %>%
   ggplot(aes(x = concrete_median, y = count)) +
   geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.7) +
-  labs(title = "Frequency of Coyote Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
+  labs(title = "Number of Coyote Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
   theme_minimal()
+
+# Group hits by unique pair ID and median type
+grouped_df <- cleaned_df[!is.na(cleaned_df$New_ID_1), ] %>%
+  group_by(Pair_ID, New_ID_1) %>%
+  summarise(count = n())
+
+# Visualize histogram of hits per median type
+ggplot(grouped_df, aes(x = New_ID_1)) +
+  geom_bar() +
+  labs(title = "Number of Hits by Median Type",
+       x = "Median Type",
+       y = "Count") +
+  theme_minimal()
+
+# Visualize histogram of hits per median pair
+ggplot(grouped_df, aes(x = Pair_ID)) +
+  geom_bar() +
+  labs(title = "Number of Hits by Median Type",
+       x = "Median Type",
+       y = "Count") +
+  theme_minimal()
+
+# Visualize stacked bar plot of hits per transect by median type
+ggplot(grouped_df, aes(x = factor(Pair_ID), y = count, fill = New_ID_1)) +
+  geom_bar(stat = "identity", position = "stack") +
+  labs(title = "Number of Hits per Road Transect by Median Type",
+       x = "Pair_ID",
+       y = "Count") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set1")
+
+
+# Perform chi-squared test to determine association
+
+# Create a contingency table
+contingency_table <- table(cleaned_df_filtered$concrete_median)
+
+# Perform the chi-squared test
+chi_squared_test <- chisq.test(contingency_table)
+
+# Print the results of the chi-squared test
+print(chi_squared_test)
