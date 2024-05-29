782153b80524f8b69f98dc5a8b9b67ee4e3545b9
bjhodgson <bjhodgson@ucdavis.edu>
2024-05-28 20:33:26 -0700

    cleanup

--- median_barriers/R/CROS_medians.R
+++ median_barriers/R/CROS_medians.R
@@ -40,86 +40,60 @@ columns_to_remove <- c(
 # Remove the specified columns
 cleaned_df <- combined_df %>% select(-one_of(columns_to_remove))
 
-# Filter out NA values from the cleaned dataframe
-cleaned_df_filtered <- cleaned_df[!is.na(cleaned_df$New_ID_1), ]
-
-# Visualize histogram of hits per median pair type
-cleaned_df_filtered %>%
-  group_by(Pair_Type) %>%
+# Histogram of total hits per median type
+cleaned_df %>%
+  group_by(New_ID_1) %>%
   summarise(count = n()) %>%
-  ggplot(aes(x = Pair_Type, y = count)) +
-  geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Frequency of 'New_ID_1' Values", x = "Value", y = "Count") +
+  ggplot(aes(x = New_ID_1, y = count, fill = New_ID_1)) +
+  geom_bar(stat = "identity") +
+  labs(x = "", y = "Total Hits", fill = "Median Type") +
   theme_minimal()
 
 # Create list of excluded median types
 exclude_values <- c("tran")
 
 # Filter out excluded median types
-cleaned_df_filtered <- cleaned_df_filtered %>%
+cleaned_df_filtered <- cleaned_df %>%
   filter(!New_ID_1 %in% exclude_values)
+
+# Histogram of number of median pair types
+cleaned_df_filtered %>%
+  group_by(Pair_Type) %>%
+  summarise(count = n()) %>%
+  ggplot(aes(x = Pair_Type, y = count)) +
+  geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.7) +
+  labs(x = "Type of Median Pair", y = "Number of Pairs") +
+  theme_minimal()
 
 # Add concrete median binary variable
 cleaned_df_filtered$concrete_median <- as.factor(ifelse(cleaned_df_filtered$New_ID_1 == "con", 1, 0))
 
-# Visualize histogram of hits per concrete vs non-concrete median type
+# Histogram of hits per concrete vs non-concrete median type
 cleaned_df_filtered %>%
   group_by(concrete_median) %>%
   summarise(count = n()) %>%
   ggplot(aes(x = concrete_median, y = count)) +
   geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Number of Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
+  labs(title = "Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Total Hits") +
   theme_minimal()
 
-# Visualize histogram of deer hits per concrete vs non-concrete median type
-cleaned_df_filtered %>%
-  filter(animal == "Mule (or Black tailed) Deer") %>%
-  group_by(concrete_median) %>%
-  summarise(count = n()) %>%
-  ggplot(aes(x = concrete_median, y = count)) +
-  geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.7) +
-  labs(title = "Number of Deer Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
-  theme_minimal()
 
-# Visualize histogram of coyote hits per concrete vs non-concrete median type
-cleaned_df_filtered %>%
-  filter(animal == "Coyote") %>%
-  group_by(concrete_median) %>%
-  summarise(count = n()) %>%
-  ggplot(aes(x = concrete_median, y = count)) +
-  geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.7) +
-  labs(title = "Number of Coyote Hits by Concrete vs Non-concrete Medians", x = "Concrete Median", y = "Count") +
-  theme_minimal()
+
 
 # Group hits by unique pair ID and median type
 grouped_df <- cleaned_df[!is.na(cleaned_df$New_ID_1), ] %>%
   group_by(Pair_ID, New_ID_1) %>%
   summarise(count = n())
 
-# Visualize histogram of hits per median type
-ggplot(grouped_df, aes(x = New_ID_1)) +
-  geom_bar() +
-  labs(title = "Number of Hits by Median Type",
-       x = "Median Type",
-       y = "Count") +
-  theme_minimal()
-
-# Visualize histogram of hits per median pair
-ggplot(grouped_df, aes(x = Pair_ID)) +
-  geom_bar() +
-  labs(title = "Number of Hits by Median Type",
-       x = "Median Type",
-       y = "Count") +
-  theme_minimal()
-
-# Visualize stacked bar plot of hits per transect by median type
+# Stacked bar plot of hits per transect by median type
 ggplot(grouped_df, aes(x = factor(Pair_ID), y = count, fill = New_ID_1)) +
   geom_bar(stat = "identity", position = "stack") +
   labs(title = "Number of Hits per Road Transect by Median Type",
-       x = "Pair_ID",
-       y = "Count") +
-  theme_minimal() +
-  scale_fill_brewer(palette = "Set1")
+       x = "Pair ID",
+       y = "Total Hits",
+       fill = "Median Type") +
+  theme_minimal() 
+
 
 # Summary statistics by grouped type
 summary_stats_medtype <- grouped_df %>%
@@ -134,6 +108,42 @@ summary_stats_medtype <- grouped_df %>%
 
 # Print the results
 print(summary_stats_medtype)
+
+# Mean difference in counts (concrete - nonconcrete)
+difference_df <- grouped_df %>%
+  filter(New_ID_1 %in% c("con", "veg", "thrie", "cab", "none")) %>% # Filter and mutate to add concrete_var category
+  mutate(concrete_var = ifelse(New_ID_1 == "con", "concrete", "nonconcrete")) %>%
+  pivot_wider(names_from = concrete_var, values_from = count, values_fill = list(count = 0)) %>% # Pivot to wide format
+  group_by(Pair_ID) %>%
+  summarise( # Summarize to get total counts for 'concrete' and 'nonconcrete' per Pair_ID
+    concrete = sum(concrete, na.rm = TRUE),
+    nonconcrete = sum(nonconcrete, na.rm = TRUE),
+    difference = concrete - nonconcrete
+  ) %>%
+  select(Pair_ID, difference) # Select desired columns for dataframe
+
+# View the data frame
+print(difference_df)
+
+# Exploratory visualizations of difference in counts 
+
+# Visualize distribution in boxplot
+boxplot(difference_df$difference)
+
+# Visualize distribution in density
+plot(density(difference_df$difference))
+# Add mean line
+abline(v = mean(final_df$difference), col = "red", lwd = 2)
+
+# Summary statistics
+summary(final_df$difference)
+
+
+
+
+
+
+
 
 # Perform chi-squared test to determine association
 
@@ -191,34 +201,7 @@ year_df %>%
   theme_bw()
 
 
-# Mean difference in counts (concrete - nonconcrete)
-difference_df <- grouped_df %>%
-  filter(New_ID_1 %in% c("con", "veg", "thrie", "cab", "none")) %>% # Filter and mutate to add concrete_var category
-  mutate(concrete_var = ifelse(New_ID_1 == "con", "concrete", "nonconcrete")) %>%
-  pivot_wider(names_from = concrete_var, values_from = count, values_fill = list(count = 0)) %>% # Pivot to wide format
-  group_by(Pair_ID) %>%
-  summarise( # Summarize to get total counts for 'concrete' and 'nonconcrete' per Pair_ID
-    concrete = sum(concrete, na.rm = TRUE),
-    nonconcrete = sum(nonconcrete, na.rm = TRUE),
-    difference = concrete - nonconcrete
-  ) %>%
-  select(Pair_ID, difference) # Select desired columns for dataframe
 
-# View the data frame
-print(difference_df)
-
-# Exploratory visualizations of difference in counts 
-
-# Visualize distribution in boxplot
-boxplot(difference_df$difference)
-
-# Visualize distribution in density
-plot(density(difference_df$difference))
-# Add mean line
-abline(v = mean(final_df$difference), col = "red", lwd = 2)
-
-# Summary statistics
-summary(final_df$difference)
 #----------
 
 # ggplot(summary_stats_medtype_by_year, aes(x=obs_year, y=mean_count, fill=New_ID_1)) +
