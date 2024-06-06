643f8b1220ba59a62d0ddb25bfe1aef69ceb68d6
bjhodgson <bjhodgson@ucdavis.edu>
2024-06-05 17:17:31 -0700

    Upload

---
+++ hotspots/bridge_search.R
@@ -0,0 +1,27 @@
+library(readxl)
+
+points_100ft <- read_excel() %>%
+  mutate(NearBridge)
+
+points_100ft$
+
+# Looping over the entire length of the data frame
+df <- points_100ft
+  
+for (i in 1:nrow(df)) {
+  row <- df[i, ]
+  
+  if row[[VCU]] == 0 {
+    df[i+53, 'NearBridge'] = 1
+    df[i-53, 'NearBridge'] = 1
+    
+    
+    
+    
+     }
+  
+ # else {df[i, 'NearBridge'] = 0
+  
+  }
+}
+
