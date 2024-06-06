35902b7a54201033e7876fc7798a3e402d8be774
bjhodgson <bjhodgson@ucdavis.edu>
2024-06-05 20:51:04 -0700

    Clean up

--- hotspots/bridge_search.R
+++ hotspots/bridge_search.R
@@ -1,42 +1,30 @@
-library(readxl)
 
-points_100ft <- read.csv("C:\\Users\\Leo Hecht\\Documents\\Road Ecology\\Hotspot Report\\top_half.csv")
-  
+# Read in csv as dataframe
+#df <- read.csv("C:\\Users\\Leo Hecht\\Documents\\Road Ecology\\Hotspot Report\\top_half.csv") # Leo path
+df <- read.csv("H:\\hotspots\\fencing_crossings\\output_data\\points_table.csv") # Ben path
 
-points_100ft$NearBridge <- 0
+# Identify bridge points
+bridge_indices <- which(df$VCU == 0)
 
-# Looping over the entire length of the data frame
-gdf <- points_100ft
-  
-# df <- read.csv('path_to_your_file.csv')
-
-
-# Step 2: Identify bridge points
-bridge_indices <- which(gdf$VCU == 0)
-
-# Step 3 & 4: Mark points within 1 mile of a bridge
-gdf$near_bridge <- 0
+# Mark points within 1 mile (53 points) of a bridge
+df$near_bridge <- 0
 
 for (bridge_idx in bridge_indices) {
+  
+  # Mark point at bridge 
+ # df$near_bridge[bridgeidx] <- 1
+  
   # Mark points within 1 mile before the bridge
   start_idx <- max(1, bridge_idx - 53)
-  gdf$near_bridge[start_idx:bridge_idx] <- 1
+  df$near_bridge[start_idx:bridge_idx] <- 1
   
   # Mark points within 1 mile after the bridge
-  end_idx <- min(nrow(gdf), bridge_idx + 53)
-  gdf$near_bridge[bridge_idx:end_idx] <- 1
+  end_idx <- min(nrow(df), bridge_idx + 53)
+  df$near_bridge[bridge_idx:end_idx] <- 1
 }
-
-
-
-
-head(gdf)
 
 # Save the edited dataset
-write.csv(gdf, "C:\\Users\\Leo Hecht\\Documents\\Road Ecology\\Hotspot Report\\edited_dataset.csv", row.names = FALSE)
-  
- # else {df[i, 'NearBridge'] = 0
-  
-  }
-}
+#write.csv(df, "C:\\Users\\Leo Hecht\\Documents\\Road Ecology\\Hotspot Report\\edited_dataset.csv", row.names = FALSE) # Leo path
+write.csv(df, "H:\\hotspots\\fencing_crossings\\output_data\\updated_points_table.csv", row.names = FALSE) # Ben path
+
 
